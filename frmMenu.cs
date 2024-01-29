f777d0d08fbdf4fe9c2577c15b6674c9f248e9f4
Sumedh_kamble <136175353+Sumedhk005@users.noreply.github.com>
2024-01-29 18:30:52 +0530

    Add files via upload

---
+++ frmMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MobileShopMaster
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginMasterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoginMaster login = new LoginMaster();
+            login.MdiParent = this;
+            login.Show();
+        }
+
+        private void productMasterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPackingMaster product = new frmPackingMaster();
+            product.MdiParent = this;
+            product.Show();
+        }
+
+        private void companyMasterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCompanyMaster company = new frmCompanyMaster();
+            company.MdiParent = this;
+            company.Show();
+        }
+
+        private void customerMasterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCustomerMaster customer = new frmCustomerMaster();   
+            customer.MdiParent = this;
+            customer.Show();
+        }
+    }
+}
